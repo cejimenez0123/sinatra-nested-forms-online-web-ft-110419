435463818befca7f08599e311a52b10dfdb5955a
Christian Jimenez <christianjimenez0123@gmail.com>
2020-01-11 18:24:41 +0000

    Done.

--- app/models/pirate.rb
+++ app/models/pirate.rb
@@ -1,10 +1,11 @@
 class Pirate
-  attr_accessor :name,:weight,:height
+  attr_accessor :name,:weight,:height, :ships
   @@pirates = []
   def initialize(hash)
     @name = hash[:name]
     @weight = hash[:weight]
     @height = hash[:height]
+    @ships = hash[:ships]
     @@pirates << self
   end
   def self.clear
